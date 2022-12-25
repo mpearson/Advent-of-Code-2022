94bbb9217c961ec3d49005c3e3cd5ffda9cbc342
Matthew Pearson <matthew.pearson@joby.aero>
2022-12-25 01:20:55 -0800

    problem 21

--- problem_21.py
+++ problem_21.py
@@ -1,48 +1,116 @@
 from pathlib import Path
-from dataclasses import dataclass
+import dataclasses
 import numpy as np
 
 data_path = "data/problem_21.txt"
 # data_path = "data/problem_21_test.txt"
 
 
-@dataclass
+@dataclasses.dataclass
 class MonkeyNode:
-
     name: str
     number: int = None
-    operator: str = None
+    listener: "MonkeyNode" = None
 
+    operator: str = None
+    monkey_1: "MonkeyNode" = None
+    monkey_2: "MonkeyNode" = None
+    dependent: bool = False
 
     @classmethod
     def from_text(cls, line):
+        tokens = line.rstrip().split(" ")
+        name = tokens[0][:-1]
+        if len(tokens) == 2:
+            return cls(name, number=int(tokens[1]))
+        else:
+            assert len(tokens) == 4
+            return cls(name, operator=tokens[2], monkey_1=tokens[1], monkey_2=tokens[3])
 
-        return cls()
 
+def load():
+    with open(data_path, "r") as f:
+        monkeys = [MonkeyNode.from_text(line) for line in f]
 
-with open(data_path, "r") as f:
-    data = f.readlines()
+    monkey_dict = {monkey.name: monkey for monkey in monkeys}
 
-# print(data)
+    for monkey in monkeys:
+        if monkey.number is None:
+            monkey.monkey_1 = monkey_dict[monkey.monkey_1]
+            monkey.monkey_2 = monkey_dict[monkey.monkey_2]
+            assert monkey.monkey_1.listener is None
+            assert monkey.monkey_2.listener is None
+            monkey.monkey_1.listener = monkey
+            monkey.monkey_2.listener = monkey
+
+    return monkey_dict
+
+monkey_dict = load()
 
 # part 1
-print(f"Part 1 solution: {None}")
+def do_the_monkey(monkey):
+    if monkey.number is None:
+        monkey_1_value = do_the_monkey(monkey.monkey_1)
+        monkey_2_value = do_the_monkey(monkey.monkey_2)
+
+        match monkey.operator:
+            case "+":
+                return monkey_1_value + monkey_2_value
+            case "-":
+                return monkey_1_value - monkey_2_value
+            case "*":
+                return monkey_1_value * monkey_2_value
+            case "/":
+                return monkey_1_value // monkey_2_value
+
+    return monkey.number
+
+print(f"Part 1 solution: {do_the_monkey(monkey_dict['root'])}")
 
 # part 2
-print(f"Part 2 solution: {None}")
+monkey_dict["root"].operator = "="
+monkey_dict["humn"].number = None
 
-# root: pppw + sjmn
-# dbpl: 5
-# cczh: sllz + lgvd
-# zczc: 2
-# ptdq: humn - dvpt
-# dvpt: 3
-# lfqf: 4
-# humn: 5
-# ljgn: 2
-# sjmn: drzm * dbpl
-# sllz: 4
-# pppw: cczh / lfqf
-# lgvd: ljgn * ptdq
-# drzm: hmdt - zczc
-# hmdt: 32
+def mark_dependent_monkeys(monkey):
+    monkey.dependent = True
+    if monkey.listener is not None:
+        mark_dependent_monkeys(monkey.listener)
+
+mark_dependent_monkeys(monkey_dict["humn"])
+
+def become_the_monkey(monkey, target_value):
+    assert monkey.number is None
+    if monkey.name == "humn":
+        return target_value
+
+    if monkey.monkey_1.dependent:
+        dependent_monkey = monkey.monkey_1
+        independent_monkey = monkey.monkey_2
+    else:
+        dependent_monkey = monkey.monkey_2
+        independent_monkey = monkey.monkey_1
+
+    match monkey.operator:
+        case "+":
+            # target = monkey_1 + monkey_2
+            return become_the_monkey(dependent_monkey, target_value - do_the_monkey(independent_monkey))
+        case "-":
+            # target = monkey_1 - monkey_2
+            if monkey.monkey_1 is dependent_monkey:
+                return become_the_monkey(dependent_monkey, target_value + do_the_monkey(independent_monkey))
+            else:
+                return become_the_monkey(dependent_monkey, do_the_monkey(independent_monkey) - target_value)
+        case "*":
+            # target = monkey_1 * monkey_2
+            return become_the_monkey(dependent_monkey, target_value // do_the_monkey(independent_monkey))
+        case "/":
+            # target = monkey_1 // monkey_2
+            if monkey.monkey_1 is dependent_monkey:
+                return become_the_monkey(dependent_monkey, target_value * do_the_monkey(independent_monkey))
+            else:
+                return become_the_monkey(dependent_monkey, do_the_monkey(independent_monkey) // target_value)
+        case "=":
+            # target = monkey_1 == monkey_2
+            return become_the_monkey(dependent_monkey, do_the_monkey(independent_monkey))
+
+print(f"Part 2 solution: {become_the_monkey(monkey_dict['root'], 1)}")
